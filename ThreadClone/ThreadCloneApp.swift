7e2569714ac9b6391ed2c5ead0aaeec47953df98
Saqib Adnan <Saqibadnan7755@gmail.com>
2023-08-15 20:22:18 +0500

    Initial Commit

---
+++ ThreadClone/ThreadCloneApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThreadCloneApp.swift
+//  ThreadClone
+//
+//  Created by apple on 15/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct ThreadCloneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
