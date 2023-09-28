0f63377542b07b470d5cab129e85cc9249844dcb
Saqib Adnan <Saqibadnan7755@gmail.com>
2023-09-29 03:30:14 +0500

    Modified: Modification in SwiftUI Section

--- ThreadClone/Core/Authentication/Views/LoginView.swift
+++ ThreadClone/Core/Authentication/Views/LoginView.swift
@@ -13,64 +13,68 @@ struct LoginView: View {
     
     var body: some View {
         NavigationStack {
-            VStack {
-                Spacer()
-                Image("thread-app-icon")
-                    .resizable()
-                    .scaledToFit()
-                    .frame(width: 130, height: 130)
-                    .padding(.vertical, 30)
-                
+            ZStack {
+                Color.black
+                    .ignoresSafeArea(.all)
                 VStack {
-                    TextField("Enter your email", text: $viewModel.email)
-                        .autocapitalization(.none)
-                        .modifier(ThreadsTextFieldModifier())
-                    
-                    SecureField("Enter your password", text: $viewModel.password)
-                        .modifier(ThreadsTextFieldModifier())
-
-                }
-                NavigationLink {
-                    Text("Forget password")
-                } label: {
-                    Text("Forgot Password?")
-                        .font(.footnote)
-                        .fontWeight(.semibold)
-                        .padding(.vertical)
-                        .padding(.trailing, 30)
-                        .foregroundColor(.black)
-                        .frame(maxWidth: .infinity, alignment: .trailing)
-                }
-                
-                Button {
-                    Task { try await viewModel.login() }
-                } label: {
-                    Text("Login")
-                        .font(.subheadline)
-                        .fontWeight(.semibold)
-                        .foregroundColor(.white)
-                        .frame(width: 330, height: 62)
+                    Spacer()
+                    Image("thread-app-icon")
+                        .resizable()
+                        .scaledToFit()
+                        .frame(width: 130, height: 130)
+                        .padding(.vertical, 30)
                         .background(.black)
-                        .cornerRadius(10)
-                }
-                Spacer()
-                Divider()
-                
-                NavigationLink {
-                    RegistrationView()
-                        .navigationBarBackButtonHidden(true)
-                } label: {
-                    HStack(spacing: 3) {
-                        Text("Don't have an account?")
+                    
+                    VStack {
+                        TextField("Enter your email", text: $viewModel.email)
+                            .autocapitalization(.none)
+                            .modifier(ThreadsTextFieldModifier())
                         
-                        Text("Sign Up")
-                            .fontWeight(.semibold)
-                    }
-                    .font(.footnote)
-                    .foregroundColor(.black)
-                }
-                .padding(.vertical, 17)
+                        SecureField("Enter your password", text: $viewModel.password)
+                            .modifier(ThreadsTextFieldModifier())
 
+                    }
+                    NavigationLink {
+                        Text("Forget password")
+                    } label: {
+                        Text("Forgot Password?")
+                            .font(.footnote)
+                            .fontWeight(.semibold)
+                            .padding(.vertical)
+                            .padding(.trailing, 30)
+                            .foregroundColor(.white)
+                            .frame(maxWidth: .infinity, alignment: .trailing)
+                    }
+                    
+                    Button {
+                        Task { try await viewModel.login() }
+                    } label: {
+                        Text("Login")
+                            .font(.subheadline)
+                            .fontWeight(.semibold)
+                            .foregroundColor(.white)
+                            .frame(width: 330, height: 62)
+                            .background(LinearGradient(gradient: Gradient(colors: [.red, .orange]), startPoint: .leading, endPoint: .trailing))
+                            .cornerRadius(10)
+                    }
+                    Spacer()
+                    Divider()
+                    
+                    NavigationLink {
+                        RegistrationView()
+                            .navigationBarBackButtonHidden(true)
+                    } label: {
+                        HStack(spacing: 3) {
+                            Text("Don't have an account?")
+                            
+                            Text("Sign Up")
+                                .fontWeight(.semibold)
+                        }
+                        .font(.footnote)
+                        .foregroundColor(.white)
+                    }
+                    .padding(.vertical, 17)
+                }
             }
         }
     }
