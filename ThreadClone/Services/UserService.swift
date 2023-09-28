be3186ed090326259fcceaefd807d58291d5824c
Saqib Adnan <Saqibadnan7755@gmail.com>
2023-09-29 03:05:48 +0500

    Modified: Implement Firebase Firestore within Swift

---
+++ ThreadClone/Services/UserService.swift
@@ -0,0 +1,27 @@
+//
+//  UserService.swift
+//  ThreadClone
+//
+//  Created by apple on 29/09/2023.
+//
+
+import Firebase
+import FirebaseFirestoreSwift
+
+class UserService {
+    @Published var currentUser: User?
+    static let shared = UserService()
+    
+    init() {
+        Task {try await fetchCurrentUser() }
+    }
+    
+    @MainActor
+    func fetchCurrentUser() async throws {
+        guard let uid = Auth.auth().currentUser?.uid else { return }
+        let snapshot = try await Firestore.firestore().collection("users").document(uid).getDocument()
+        
+        let user = try snapshot.data(as: User.self)
+        self.currentUser = user
+    }
+}
