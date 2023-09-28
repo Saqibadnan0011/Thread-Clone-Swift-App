7bbd546a0183d3f1d22c60af8d8779cd08b4a650
Saqib Adnan <Saqibadnan7755@gmail.com>
2023-09-29 03:04:18 +0500

    Modified: Implement combine functionality for setup the subscribers methods

---
+++ ThreadClone/Core/Profile/ViewModel/ProfileViewModel.swift
@@ -0,0 +1,25 @@
+//
+//  ProfileViewModel.swift
+//  ThreadClone
+//
+//  Created by apple on 29/09/2023.
+//
+
+import Foundation
+import Combine
+
+class ProfileViewModel: ObservableObject {
+    @Published var currentUser: User?
+    private var cancellables = Set<AnyCancellable>()
+    
+    init () {
+        setupSubscribers()
+    }
+    
+    private func setupSubscribers() {
+        UserService.shared.$currentUser.sink { [weak self] user in
+            self?.currentUser = user
+            print("DEBUG: bla bla bla\(user)")
+        }.store(in: &cancellables)
+    }
+}
