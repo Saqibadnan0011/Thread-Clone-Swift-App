bd82050b45e0e36e000fe3fe1ba66b5c3ed6ceb4
Saqib Adnan <Saqibadnan7755@gmail.com>
2023-09-30 02:09:53 +0500

    Added fetching func

---
+++ ThreadClone/Core/Explore/ViewModel/ExploreViewModel.swift
@@ -0,0 +1,21 @@
+//
+//  ExploreViewModel.swift
+//  ThreadClone
+//
+//  Created by apple on 30/09/2023.
+//
+
+import Foundation
+
+class ExploreViewModel: ObservableObject {
+    @Published var users = [User]()
+    
+    init() {
+        Task { try await fetchUsers() }
+    }
+    
+    @MainActor
+    private func fetchUsers() async throws {
+        self.users = try await UserService.fetchUsers()
+    }
+}
