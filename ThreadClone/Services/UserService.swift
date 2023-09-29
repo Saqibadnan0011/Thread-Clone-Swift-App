aa661681517c29141cf2bb8df867fbb94f4ef3c1
Saqib Adnan <Saqibadnan7755@gmail.com>
2023-09-30 02:09:09 +0500

    Implement fetchuser function

--- ThreadClone/Services/UserService.swift
+++ ThreadClone/Services/UserService.swift
@@ -25,6 +25,13 @@ class UserService {
         self.currentUser = user
     }
     
+    static func fetchUsers() async throws -> [User] {
+        guard let currentUid = Auth.auth().currentUser?.uid else { return [] }
+        let snapshot  = try await Firestore.firestore().collection("users").getDocuments()
+        let users = snapshot.documents.compactMap({ try? $0.data(as: User.self)})
+        return users.filter({ $0.id != currentUid })
+    }
+    
     func reset() {
         self.currentUser = nil
     }
