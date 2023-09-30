3e90b2a978127d3f5865e4a4ac1ef16bbfa8dda9
Saqib Adnan <Saqibadnan7755@gmail.com>
2023-09-30 14:21:24 +0500

    Modified: Updated thread views

--- ThreadClone/Core/ThreadCreation/View/CreateThreadView.swift
+++ ThreadClone/Core/ThreadCreation/View/CreateThreadView.swift
@@ -12,55 +12,61 @@ struct CreateThreadView: View {
     
     var body: some View {
         NavigationStack {
-            VStack {
-                HStack(alignment: .top, spacing: 4) {
-                    CircularProfileImageView()
-                    
-                    VStack(alignment: .leading) {
-                        Text("Rana Danish")
-                            .fontWeight(.semibold)
-                        TextField("Create a thread", text: $caption, axis: .vertical)
-                    }
-                    .padding(.horizontal, 10)
-                    .font(.footnote)
-                    Spacer()
-                    
-                    if !caption.isEmpty {
-                        Button {
-                            caption = ""
-                        } label: {
-                            Image(systemName: "xmark")
-                                .resizable()
-                                .frame(width: 12, height: 12)
-                                .foregroundColor(.black).opacity(0.5)
+            ZStack {
+                Color.black
+                    .ignoresSafeArea(.all)
+                VStack {
+                    HStack(alignment: .top, spacing: 4) {
+                        CircularProfileImageView()
+                        
+                        VStack(alignment: .leading) {
+                            Text("Rana Danish")
+                                .fontWeight(.semibold)
+                            TextField("Create a thread", text: $caption, axis: .vertical)
+                                .foregroundColor(.white).opacity(0.7)
+                        }
+                        //.padding(.horizontal, 10)
+                        .font(.footnote)
+                        Spacer()
+                        
+                        if !caption.isEmpty {
+                            Button {
+                                caption = ""
+                            } label: {
+                                Image(systemName: "xmark")
+                                    .resizable()
+                                    .frame(width: 12, height: 12)
+                                    .foregroundColor(.white).opacity(0.5)
+                            }
                         }
                     }
+                    Spacer()
                 }
-                Spacer()
-            }
-            .padding(30)
-            .toolbar {
-                ToolbarItem(placement: .navigationBarLeading) {
-                    Button ("Cancel") {
-                        dismiss()
+                .padding(30)
+                .toolbar {
+                    ToolbarItem(placement: .navigationBarLeading) {
+                        Button ("Cancel") {
+                            dismiss()
+                        }
+                        .font(.footnote)
+                        .foregroundColor(.white)
                     }
-                    .font(.footnote)
-                    .foregroundColor(.black)
-                }
-                
-                ToolbarItem(placement: .navigationBarTrailing) {
-                    Button ("Post") {
-                        
+                    
+                    ToolbarItem(placement: .navigationBarTrailing) {
+                        Button ("Post") {
+                            
+                        }
+                        .opacity(caption.isEmpty ? 0.5 : 1.0)
+                        .disabled(caption.isEmpty)
+                        .font(.footnote)
+                        .fontWeight(.semibold)
+                        .foregroundColor(.white)
                     }
-                    .opacity(caption.isEmpty ? 0.5 : 1.0)
-                    .disabled(caption.isEmpty)
-                    .font(.footnote)
-                    .fontWeight(.semibold)
-                    .foregroundColor(.black)
                 }
+                .navigationTitle("Create Thread")
+                .navigationBarTitleDisplayMode(.inline)
+                .foregroundColor(.white)
             }
-            .navigationTitle("Create Thread")
-            .navigationBarTitleDisplayMode(.inline)
         }
     }
 }
